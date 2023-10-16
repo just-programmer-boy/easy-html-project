d53c3e7cfba344a11d18d3c31fbcde9dfcc75b31
just-programmer-boy <144595703+just-programmer-boy@users.noreply.github.com>
2023-10-16 22:27:29 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project #2</title>
+</head>
+<body style="background-color:black;">
+    <img src="https://images.unsplash.com/photo-1565206595640-6cfd90e0ae09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhlbGxvJTIwd29ybGR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60" alt="Hello world" height="500" style="width:100%px">
+    <style>
+        img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 1000px
+          }
+    </style>
+
+    <hr>
+    <p style="color:crimson; text-align:center ; font-size:25px">
+        This is really simple html project but with a text-align,font-size,horizontal line as you my have noticed! If you want to use it make sure to 
+    </p>
+<hr>
+    <p style="color:crimson; text-align:center; font-size:40px;">
+        Download
+    </p>
+<hr>
+    <p style="color:crimson;text-align:center; font-size:25px">
+        Codes from my profile
+    </p>
+<hr>
+    <a href="https://github.com/just-programmer-boy">Don't forget to subscribe!</a>
+
+</body>
+</html>
+# If there are some issues that you are facing you can contact me in the e-mail on my info
